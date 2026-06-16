fadc803fe9750c602f620b5135e8afd39fc714c1
Mathis <mathislamartiniere@outlook.fr>
2026-06-16 12:58:00 +0200

    last commit

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const test = require('node:test');
 
 const app = express();
 
-const joueur = require('./base.json')
+const joueur = require('./base.json');
+const { platform } = require('node:os');
 
 app.get('/api/player/random/', (req, res) => {
 
-    const randomIndex = Math.floor(Math.random()* joueur.length)
+    const randomIndex = Math.floor(Math.random() * joueur.length)
 
-    
+
     const randomPlayer = joueur[randomIndex];
 
     return res.status(200).json(randomPlayer);
@@ -21,15 +22,34 @@ app.get('/api/player/random/', (req, res) => {
 });
 
 
-
 app.post('/api/player/check', (req, res) => {
+    
+    const idJoueur = req.body.playerId;
+    const reponseDonnee = req.body.answer; 
 
-    if (!joueur) (
-        res.status(404).json("message : joueur non trouvé")
-    )
+   
+    const joueurTrouve = joueurs.find((user) => user.id === idJoueur);
 
-    return res.status(200).json({ test })
+   
+    if (!joueurTrouve) {
+        return res.status(404).json({ error: "Joueur non trouvé" });
+    }
+
+   
+    if (reponseDonnee === joueurTrouve.team) {
+        return res.status(200).json({
+            "correct": true,
+            "expected": joueurTrouve.team
+        });
+    } else {
+        return res.status(200).json({
+            "correct": false,
+            "expected": joueurTrouve.team 
+        });
+    }
 });
+
+
 
 app.listen(3000, () => {
     console.log("serveur démarré sur localhost:3000");
